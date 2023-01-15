2288d183aa0d84f3623e708dacb99e76985e561f
Musi <haha@.com>
2023-01-15 20:59:41 +0800

    第一页

--- code/miniProgram/app.js
+++ code/miniProgram/app.js
@@ -5,14 +5,8 @@ App({
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
   },
+
   globalData: {
     userInfo: null
   }
